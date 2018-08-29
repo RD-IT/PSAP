7a18aa7162aff4ca6a4024686cd5e89dc36ad25d
解西营 <xiexiying@rongda-tech.com>
2018-08-29 09:02:11 +0800

    1

--- PSAP/VIEW/BSVIEW/LoginForm.Designer.cs
+++ PSAP/VIEW/BSVIEW/LoginForm.Designer.cs
@@ -34,7 +34,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(159, 368);
+            this.button1.Location = new System.Drawing.Point(159, 304);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -43,7 +43,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(253, 367);
+            this.button2.Location = new System.Drawing.Point(253, 303);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
