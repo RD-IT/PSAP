95314be8072c238010d8da9a781bc36d5717bd50
rongda <rongda@DESKTOP-SPM8169>
2018-11-22 10:08:32 +0800

    吴璠

---
+++ PSAP/VIEW/PURVIEW/FrmPrReq.cs
@@ -0,0 +1,24 @@
+﻿using PSAP.BLL.BSBLL;
+using PSAP.DAO.BSDAO;
+using PSAP.PSAPCommon;
+using PsapUserControlLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using WeifenLuo.WinFormsUI.Docking;
+
+namespace PSAP.VIEW.BSVIEW
+{
+    public partial class FrmPrReq : DockContent
+    {
+        public FrmPrReq()
+        {
+            InitializeComponent();
+        }
+    }
+}
